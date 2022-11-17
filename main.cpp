f7bff7dfbc073a64ff5a586b0e55d2858005bf4a
FerraristaAccanito <117997140+FerraristaAccanito@users.noreply.github.com>
2022-11-17 19:28:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   
+   float a,b;
+   int opzione;
+   
+   cin >> a >> b;
+   
+   switch(opzione) {
+      case 0 :
+         cout << (a*b)/2;
+         break;
+      case 1 :
+         cout << a*a;
+         break;
+      case 2 :
+         cout << a*b;
+         break;
+      default :
+         cout << "l'opzione selezionata non è valida";
+   }
    return 0;
 }
